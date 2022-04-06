e08b64c2083899851024d6d1fe9335be73e10e3f
iwalkbyfaith <godownthestreet@naver.com>
2022-04-06 15:44:37 +0900

    22.04.06 스프링에서 DB select, insert, delete, update하기

--- SpringBoard/src/main/java/com/ict/mapper/BoardMapper.java
+++ SpringBoard/src/main/java/com/ict/mapper/BoardMapper.java
@@ -2,7 +2,9 @@ package com.ict.mapper;
 
 import java.util.List;
 
-import com.ict.vo.BoardVO;
+import org.apache.ibatis.annotations.Param;
+
+import com.ict.domain.BoardVO;
 
 public interface BoardMapper {
 
@@ -11,7 +13,7 @@ public interface BoardMapper {
 	
 	// 인터페이스에서는 
 		// 1. 리턴값(리턴 되는지 아닌지, 어떤 자료형이 리턴되는지)
-		// 2. 메서드 이름이 뭔지 생각하면 된다
+		// 2. 메서드 이름이 뭔지 생각하면 된다 (추상 메서드 배치)
 	
 	
 	// ■ Select ("SELECT * FROM board_tbl WHERE bno < 4")
@@ -20,5 +22,26 @@ public interface BoardMapper {
 	
 	// ■ insert
 	// insert 구문 실행용으로 메서드를 선언합니다.
-	// VO 내부에 적혀있는 정보를 이용해 insert를 합니다.
+	// VO 내부에 적혀있는 정보를 이용해 insert를 합니다. (String title, Strimng content... 이렇게 받아도 됨)
+	// 파라미터값이 여러개인 경우는 vo를 쓰고, 1개/2개 밖에 안되면 그냥 각각 쓰는게 나을 듯 하다.
+	// BoardVO를 매개로 insert 정보를 전달받음.
+	public void insert(BoardVO vo);
+	
+	
+	// ■ SELECT (특정 글번호 long bno을 입력 받아 콘솔에 찍어주는 메서드)
+	public BoardVO getBoard(long bno);
+	
+	
+	// ■ DELETE ( 특정 글 번호를 하나 입력 받아 글을 삭제하는 메서드 )
+	public void deleteBoard(long bno);
+	
+	
+	// ■ UPDATE (혼자 실습, 해당 글 번호 게시글 수정 - title, bno 받음)
+	public void updateBoard(@Param("title")String title, @Param("bno")long bno);
+	
+	// ■ UPDATE2 (혼자 실습, 해당 글 번호 게시글 수정 - vo로 받음)
+	public void updateBoard2(BoardVO vo);
+	
+	
+	
 }
--- SpringBoard/src/test/java/com/ict/persistence/BoardMapperTests.java
+++ SpringBoard/src/test/java/com/ict/persistence/BoardMapperTests.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.ict.domain.BoardVO;
 import com.ict.mapper.BoardMapper;
 
 import lombok.extern.log4j.Log4j;
@@ -15,13 +16,74 @@ import lombok.extern.log4j.Log4j;
 @Log4j
 public class BoardMapperTests {
 	
-	@Autowired
+	@Autowired // 어노테이션으로 처리하면 빈즈그래프에는 안보임(하지만 있는 상태)
 	private BoardMapper boardMapper;
 	
-	@Test
+	
+	
+	//@Test // 모든 글 리스트
 	public void getBoardList() {
 		log.info("getBoardList() 실행");
 		log.info(boardMapper.getList());
+		
+		// 이렇게 해도 됨
+		// List<BoardVO> result = boardMapper.getList();
+		// log.info("저장된 게시물 정보 : " + result);
+	}
+	
+	//@Test // 데이터 적재
+	public void testInsert() {
+	
+		// vo를 입력받는 insert 메서드 특성상, title, content, writer가 채워진 vo를 먼저 생성해야합니다.
+		BoardVO vo = new BoardVO();
+		
+			// 디버깅
+			log.info("채워넣기 전 : " + vo);
+		
+		// 데이터 넣어줌 (여기에서는 DB에 들어가는게 아님, 생성된 vo에 들어가는 것)
+		vo.setTitle("새로넣는제목5");
+		vo.setContent("새로넣는본문5");
+		vo.setWriter("새로넣는 글쓴이5");
+			
+			// 디버깅
+			log.info("채워넣은 후 : " + vo);
+		
+		// vo 내부에 데이터가 바인딩 된 것 확인했으니 메서드 호출 
+			// 위에 생성된 vo를 메서드 파라미터에 넣어줌 (xml로 이동해서 DB에 적재됨)
+		boardMapper.insert(vo);
+	
+	}
+	
+	
+	//@Test // 한개의 글 가져오기
+	public void getBoard() {
+		
+		log.info(boardMapper.getBoard(4));
+	}
+	
+	
+	// @Test // 한개의 글 삭제하기
+	public void testDeleteBoard() {
+		
+		boardMapper.deleteBoard(23);
+	}
+	
+	
+	//@Test  // 한 개의 글 수정하기(타이틀, 글번호 받음)
+	public void testUpdateBoard() {
+		boardMapper.updateBoard("글제목수정", 4);
+	}
+	
+	@Test // 한 개의 글 수정하기 (BoardVO 받음)
+	public void testUpdateBoard2() {
+		BoardVO vo = new BoardVO();
+		
+		vo.setTitle("vo로바꾼제목");
+		vo.setContent("vo로 바꾼 내용");
+		vo.setWriter("vo로바꾼글쓴이");
+		vo.setBno(26);
+		
+		boardMapper.updateBoard2(vo);
 	}
 
 }
